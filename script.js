5d47aefc337577d2c2978bff165cf09847f0c37a
rohitrawat6178 <125044847+rohitrawat6178@users.noreply.github.com>
2024-02-08 11:54:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,278 @@
+// other themes to add ?
+// bigger memory?
+
+
+
+
+var library = {
+    Cartoons: [
+        'https://freepngimg.com/thumb/cartoon/3-2-cartoon-free-png-image-thumb.png',
+        'https://freepngimg.com/thumb/cartoon/4-2-cartoon-transparent-thumb.png',
+        'https://freepngimg.com/thumb/cartoon/36530-9-cartoon-hd-thumb.png',
+        'https://freepngimg.com/thumb/cartoon/10-2-cartoon-png-hd-thumb.png',
+        'https://freepngimg.com/thumb/cartoon/80613--free-hq-image-thumb.png',
+        'https://freepngimg.com/thumb/cartoon/84538-story-toy-behavior-sheriff-play-game-video-thumb.png',
+        'https://freepngimg.com/thumb/cartoon/92511-behavior-bros-hand-mario-human-super-thumb.png',
+        'https://freepngimg.com/thumb/cartoon/58977-ariel-princess-disney-free-download-png-hq-thumb.png',
+        'https://freepngimg.com/thumb/cartoon/89921--free-download-image-thumb.png',
+        'https://freepngimg.com/thumb/cartoon/81982--download-hd-png-thumb.png',
+        'https://freepngimg.com/thumb/cartoon/3-2-cartoon-free-png-image-thumb.png',
+        'https://freepngimg.com/thumb/cartoon/4-2-cartoon-transparent-thumb.png',
+        'https://freepngimg.com/thumb/cartoon/36530-9-cartoon-hd-thumb.png',
+        'https://freepngimg.com/thumb/cartoon/10-2-cartoon-png-hd-thumb.png',
+        'https://freepngimg.com/thumb/cartoon/80613--free-hq-image-thumb.png',
+        'https://freepngimg.com/thumb/cartoon/84538-story-toy-behavior-sheriff-play-game-video-thumb.png',
+        'https://freepngimg.com/thumb/cartoon/92511-behavior-bros-hand-mario-human-super-thumb.png',
+        'https://freepngimg.com/thumb/cartoon/58977-ariel-princess-disney-free-download-png-hq-thumb.png',
+        'https://freepngimg.com/thumb/cartoon/89921--free-download-image-thumb.png',
+        'https://freepngimg.com/thumb/cartoon/81982--download-hd-png-thumb.png'
+    ],
+    Numbers: [
+        'https://i.pinimg.com/736x/98/d3/ca/98d3cab767ca1a9e9c048b0b97f30394.jpg',
+        'https://i.pinimg.com/564x/59/78/bc/5978bc0ae91530518643038eb75e16d5.jpg',
+        'https://i.pinimg.com/564x/82/47/4d/82474dcc2e32d9679de4c0bceb0f67c0.jpg',
+        'https://i.pinimg.com/564x/62/0c/ef/620cef13890e2f1ae2a32ccb7530d918.jpg',
+        'https://i.pinimg.com/474x/62/e1/cb/62e1cbdaa65284c42c32ade4d41cd7ab.jpg',
+        'https://i.pinimg.com/474x/50/ac/a5/50aca565a8745db4b2ab707f0a85100e.jpg',
+        'https://i.pinimg.com/474x/af/9a/dd/af9add74b6d5e2161c01b990e73d9389.jpg',
+        'https://i.pinimg.com/474x/8f/25/c5/8f25c51c11511fe765c77da9ea898e38.jpg',
+        'https://i.pinimg.com/474x/c5/01/d7/c501d7edd3bb497019e2ca347f87f381.jpg',
+        'https://i.pinimg.com/474x/24/4a/fe/244afec5a27cf4647651ddf42e4d9bc7.jpg',
+        'https://i.pinimg.com/736x/98/d3/ca/98d3cab767ca1a9e9c048b0b97f30394.jpg',
+        'https://i.pinimg.com/564x/59/78/bc/5978bc0ae91530518643038eb75e16d5.jpg',
+        'https://i.pinimg.com/564x/82/47/4d/82474dcc2e32d9679de4c0bceb0f67c0.jpg',
+        'https://i.pinimg.com/564x/62/0c/ef/620cef13890e2f1ae2a32ccb7530d918.jpg',
+        'https://i.pinimg.com/474x/62/e1/cb/62e1cbdaa65284c42c32ade4d41cd7ab.jpg',
+        'https://i.pinimg.com/474x/50/ac/a5/50aca565a8745db4b2ab707f0a85100e.jpg',
+        'https://i.pinimg.com/474x/af/9a/dd/af9add74b6d5e2161c01b990e73d9389.jpg',
+        'https://i.pinimg.com/474x/8f/25/c5/8f25c51c11511fe765c77da9ea898e38.jpg',
+        'https://i.pinimg.com/474x/c5/01/d7/c501d7edd3bb497019e2ca347f87f381.jpg',
+        'https://i.pinimg.com/474x/24/4a/fe/244afec5a27cf4647651ddf42e4d9bc7.jpg'
+    ],
+    Alphabets: [
+        'https://thumbs.dreamstime.com/z/cartoon-alphabet-illustration-123696774.jpg',
+        'https://thumbs.dreamstime.com/b/cartoon-b-alphabet-illustration-123696818.jpg',
+        'https://thumbs.dreamstime.com/b/illustration-cartoon-c-alphabet-cartoon-c-alphabet-123696816.jpg',
+        'https://i.pinimg.com/564x/63/ae/f1/63aef169b8451899e9a3edf8b9866b00.jpg',
+        'https://thumbs.dreamstime.com/b/cartoon-e-alphabet-illustration-123696827.jpg',
+        'https://thumbs.dreamstime.com/b/illustration-cartoon-f-alphabet-123696886.jpg',
+        'https://thumbs.dreamstime.com/b/cartoon-g-alphabet-illustration-123696834.jpg',
+        'https://thumbs.dreamstime.com/b/illustration-cartoon-h-alphabet-cartoon-h-alphabet-123696865.jpg',
+        'https://thumbs.dreamstime.com/b/illustration-cartoon-i-alphabet-cartoon-i-alphabet-123696881.jpg',
+        'https://thumbs.dreamstime.com/b/illustration-cartoon-j-alphabet-cartoon-j-alphabet-123696880.jpg',
+        'https://thumbs.dreamstime.com/z/cartoon-alphabet-illustration-123696774.jpg',
+        'https://thumbs.dreamstime.com/b/cartoon-b-alphabet-illustration-123696818.jpg',
+        'https://thumbs.dreamstime.com/b/illustration-cartoon-c-alphabet-cartoon-c-alphabet-123696816.jpg',
+        'https://i.pinimg.com/564x/63/ae/f1/63aef169b8451899e9a3edf8b9866b00.jpg',
+        'https://thumbs.dreamstime.com/b/cartoon-e-alphabet-illustration-123696827.jpg',
+        'https://thumbs.dreamstime.com/b/illustration-cartoon-f-alphabet-123696886.jpg',
+        'https://thumbs.dreamstime.com/b/cartoon-g-alphabet-illustration-123696834.jpg',
+        'https://thumbs.dreamstime.com/b/illustration-cartoon-h-alphabet-cartoon-h-alphabet-123696865.jpg',
+        'https://thumbs.dreamstime.com/b/illustration-cartoon-i-alphabet-cartoon-i-alphabet-123696881.jpg',
+        'https://thumbs.dreamstime.com/b/illustration-cartoon-j-alphabet-cartoon-j-alphabet-123696880.jpg'
+
+    ],
+    Colours: [
+        'https://i.pinimg.com/474x/18/4b/87/184b87082fdef0345977b9187bdf586d.jpg',
+        'https://i.pinimg.com/474x/48/b6/b6/48b6b6c953adeae6ea3882b6c580b121.jpg',
+        'https://i.pinimg.com/564x/62/ac/1f/62ac1fc460ed7d09116377e480b637ad.jpg',
+        'https://i.pinimg.com/474x/31/20/ec/3120ecd0f87255e7a49a1d0c26b10e16.jpg',
+        'https://i.pinimg.com/474x/bd/fe/31/bdfe31564246bf7ee4feb24c30675627.jpg',
+        'https://i.pinimg.com/474x/1f/c6/0c/1fc60c4ef1dd73aa988c2e3ca0979acc.jpg',
+        'https://i.pinimg.com/474x/58/76/1c/58761c010c86242a0e333575d65cf930.jpg',
+        'https://cdn1.vectorstock.com/i/thumb-large/92/00/learn-the-color-brown-things-that-are-brown-vector-22699200.jpg',
+        'https://cdn2.vectorstock.com/i/thumb-large/91/81/learn-the-color-gray-things-that-are-gray-color-vector-22699181.jpg',
+        'https://i.pinimg.com/474x/1f/16/4c/1f164cd0c2ef27ea28881e45150ded0a.jpg',
+        'https://i.pinimg.com/474x/18/4b/87/184b87082fdef0345977b9187bdf586d.jpg',
+        'https://i.pinimg.com/474x/48/b6/b6/48b6b6c953adeae6ea3882b6c580b121.jpg',
+        'https://i.pinimg.com/564x/62/ac/1f/62ac1fc460ed7d09116377e480b637ad.jpg',
+        'https://i.pinimg.com/474x/31/20/ec/3120ecd0f87255e7a49a1d0c26b10e16.jpg',
+        'https://i.pinimg.com/474x/bd/fe/31/bdfe31564246bf7ee4feb24c30675627.jpg',
+        'https://i.pinimg.com/474x/1f/c6/0c/1fc60c4ef1dd73aa988c2e3ca0979acc.jpg',
+        'https://i.pinimg.com/474x/58/76/1c/58761c010c86242a0e333575d65cf930.jpg',
+        'https://cdn1.vectorstock.com/i/thumb-large/92/00/learn-the-color-brown-things-that-are-brown-vector-22699200.jpg',
+        'https://cdn2.vectorstock.com/i/thumb-large/91/81/learn-the-color-gray-things-that-are-gray-color-vector-22699181.jpg',
+        'https://i.pinimg.com/474x/1f/16/4c/1f164cd0c2ef27ea28881e45150ded0a.jpg',
+    ],
+    Shapes: [
+        'https://thumbs.dreamstime.com/b/vector-illustration-cute-geometric-figures-kids-orange-shape-rectangle-isolated-white-background-vector-illustration-cute-144284256.jpg',
+        'https://thumbs.dreamstime.com/z/vector-illustration-cute-geometric-figures-kids-yellow-shape-star-isolated-white-background-funny-face-144284159.jpg',
+        'https://thumbs.dreamstime.com/b/vector-illustration-cute-geometric-figures-kids-green-shape-square-isolated-white-background-vector-illustration-cute-144284378.jpg',
+        'https://thumbs.dreamstime.com/b/vector-illustration-cute-geometric-figures-kids-violet-shape-triangle-isolated-white-background-vector-illustration-cute-144284458.jpg',
+        'https://thumbs.dreamstime.com/b/vector-illustration-cute-geometric-figures-kids-orange-shape-oval-isolated-white-background-vector-illustration-cute-144284484.jpg',
+        'https://thumbs.dreamstime.com/b/vector-illustration-cute-geometric-figures-kids-red-shape-circle-isolated-white-background-funny-face-vector-144284531.jpg',
+        'https://amaxkids.com/wp-content/gallery/049-freebie-printable-flashcards-for-kids-shapes/Shapes.Heart_.png',
+        'https://amaxkids.com/wp-content/gallery/049-freebie-printable-flashcards-for-kids-shapes/Shapes.Hexagon.png',
+        'https://amaxkids.com/wp-content/gallery/049-freebie-printable-flashcards-for-kids-shapes/Shapes.Diamond.png',
+        'https://ecdn.teacherspayteachers.com/thumbitem/Cone-3D-Shape-1627958-1586901259/original-1627958-2.jpg',
+        'https://thumbs.dreamstime.com/b/vector-illustration-cute-geometric-figures-kids-orange-shape-rectangle-isolated-white-background-vector-illustration-cute-144284256.jpg',
+        'https://thumbs.dreamstime.com/z/vector-illustration-cute-geometric-figures-kids-yellow-shape-star-isolated-white-background-funny-face-144284159.jpg',
+        'https://thumbs.dreamstime.com/b/vector-illustration-cute-geometric-figures-kids-green-shape-square-isolated-white-background-vector-illustration-cute-144284378.jpg',
+        'https://thumbs.dreamstime.com/b/vector-illustration-cute-geometric-figures-kids-violet-shape-triangle-isolated-white-background-vector-illustration-cute-144284458.jpg',
+        'https://thumbs.dreamstime.com/b/vector-illustration-cute-geometric-figures-kids-orange-shape-oval-isolated-white-background-vector-illustration-cute-144284484.jpg',
+        'https://thumbs.dreamstime.com/b/vector-illustration-cute-geometric-figures-kids-red-shape-circle-isolated-white-background-funny-face-vector-144284531.jpg',
+        'https://amaxkids.com/wp-content/gallery/049-freebie-printable-flashcards-for-kids-shapes/Shapes.Heart_.png',
+        'https://amaxkids.com/wp-content/gallery/049-freebie-printable-flashcards-for-kids-shapes/Shapes.Hexagon.png',
+        'https://amaxkids.com/wp-content/gallery/049-freebie-printable-flashcards-for-kids-shapes/Shapes.Diamond.png',
+        'https://ecdn.teacherspayteachers.com/thumbitem/Cone-3D-Shape-1627958-1586901259/original-1627958-2.jpg'
+
+    ],
+    Animals: [
+        'https://www.bforball.com/image/categ/camel.png',
+        'https://www.bforball.com/image/main/horse.png',
+        'https://www.bforball.com/image/main/tiger.png',
+        'https://www.bforball.com/image/main/chimpanzee.png',
+        'https://www.bforball.com/image/main/elephant.png',
+        'https://www.bforball.com/image/main/lion.png',
+        'https://www.bforball.com/image/main/dolphin.png',
+        'https://www.bforball.com/image/main/crocodile.png',
+        'https://www.bforball.com/image/main/turtle.png',
+        'https://www.bforball.com/image/main/monkey.png',
+        'https://www.bforball.com/image/categ/camel.png',
+        'https://www.bforball.com/image/main/horse.png',
+        'https://www.bforball.com/image/main/tiger.png',
+        'https://www.bforball.com/image/main/chimpanzee.png',
+        'https://www.bforball.com/image/main/elephant.png',
+        'https://www.bforball.com/image/main/lion.png',
+        'https://www.bforball.com/image/main/dolphin.png',
+        'https://www.bforball.com/image/main/crocodile.png',
+        'https://www.bforball.com/image/main/turtle.png',
+        'https://www.bforball.com/image/main/monkey.png'
+    ]
+
+
+}
+
+var images = [];
+var tempElt1 = "";
+var tempElt2 = "";
+var click = -1;
+var win = 0;
+var score = 0;
+var time = 0;
+
+var preElt = document.querySelector("#pre");
+var themesElt = document.querySelector("#themes");
+var boxElts = document.getElementsByClassName("box");
+var mainElt = document.querySelector(".main");
+var timeElt = document.querySelector("#time");
+var scoreElt = document.querySelector("#score");
+var postElt = document.querySelector("#post");
+var finalElt = document.querySelector("#final");
+var againElt = document.querySelector("#again");
+
+
+// initiate the game with chosen theme
+themesElt.addEventListener("click", function(e) {
+    if (e.target.classList.contains("themes")) {
+        activateTheme(e.target.id);
+        preElt.classList.add("hidden");
+    }
+});
+
+
+function activateTheme(theme) {
+    // insert theme in images array
+    for (let i = 0; i < 20; i++) { images.push(library[theme][i]); }
+    // insert images in memory game
+    for (let i = 0; i < 20; i++) {
+        var rand = Math.floor(Math.random() * (images.length - 1));
+        boxElts[i].innerHTML = "<img src='" + images[rand] + "' alt='image' class='hidden'>";
+        images.splice(rand, 1);
+    }
+}
+
+
+// Handle the play
+mainElt.addEventListener("click", gameLogic);
+
+function gameLogic(e) {
+    // make sure the box is playable
+    if (e.target.classList.contains("play")) {
+        e.target.firstChild.classList.remove("hidden");
+        // first of two click
+        if (click < 1) {
+            tempElt1 = e.target;
+            // timer
+            if (click === -1) {
+                timer = setInterval(function() {
+                    time++;
+                    timeElt.innerHTML = time;
+                }, 1000);
+            }
+            click = 1;
+        }
+
+        // second click
+        else if (e.target !== tempElt1) {
+            tempElt2 = e.target;
+
+            // different images
+            if (tempElt1.firstChild.src !== tempElt2.firstChild.src) {
+                mainElt.removeEventListener("click", gameLogic);
+                setTimeout(function() {
+                    tempElt1.firstChild.classList.add("hidden");
+                    tempElt2.firstChild.classList.add("hidden");
+                    mainElt.addEventListener("click", gameLogic);
+                }, 400);
+                if (score > 0) {
+                    score -= 2;
+                }
+                scoreElt.innerHTML = score;
+            }
+
+            // same images
+            else {
+                score += 10;
+                win += 2;
+                tempElt1.firstChild.classList.add("outlined");
+                tempElt2.firstChild.classList.add("outlined");
+                tempElt1.classList.remove("play");
+                tempElt2.classList.remove("play");
+                scoreElt.innerHTML = score;
+
+                // game won
+                if (win === 20) {
+                    clearInterval(timer);
+                    finalElt.innerHTML = "You won " + score + " points <br> in " + time + " seconds";
+                    postElt.classList.remove("hidden");
+                }
+            }
+            click = 0;
+        }
+    }
+}
+
+againElt.addEventListener("click", resetGame);
+
+function resetGame() {
+    // reset game
+    tempElt1 = "";
+    tempElt2 = "";
+    click = -1;
+    win = 0;
+    score = 0;
+    time = 0;
+    postElt.classList.add("hidden");
+    preElt.classList.remove("hidden");
+    for (let i = 0; i < 20; i++) {
+        boxElts[i].classList.add("play");
+        boxElts[i].firstChild.classList.add("hidden");
+    }
+    timeElt.textContent = time;
+    scoreElt.textContent = score;
+}
+
+document.getElementById("reset").addEventListener("click", () => location.reload());
+
+
+// handle focus of the page
+// function checkPageFocus() {
+//   if (document.hasFocus()) {
+//     preElt.classList.remove("hidden");
+//   }
+//   else {
+//     preElt.classList.add("hidden");
+//   }
+// }
+// var checkPageInterval = setInterval(checkPageFocus, 300);
